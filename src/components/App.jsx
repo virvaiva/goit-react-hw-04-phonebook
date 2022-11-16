40ef24bfdbaa03dcb8b8acdfb0f59735458b5c78
Oksana Konieva <anaska0489@gmail.com>
2022-11-16 09:10:25 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,14 +8,7 @@ import { Filter } from './Filter/Filter';
 
 export function App() {
   const [contacts, setcontacts] = useState(
-    // () =>
-    //   JSON.parse(window.localStorage.getItem('contacts')) ?? [
-    [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ]
+    () => JSON.parse(window.localStorage.getItem('contacts')) ?? []
   );
   const [filter, setFilter] = useState('');
 
